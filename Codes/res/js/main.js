6e0246c6766bb271e50ea5a93044f03eac18d9d6
Mbaye Faye ndour <mbfndour@gmail.com>
2024-01-20 17:04:36 +0000

    first commit

---
+++ Codes/res/js/main.js
@@ -0,0 +1,11 @@
+var a = prompt("quel est votre nom")
+var b = prompt("quel est votre prenom")
+var c = prompt("votre date de naissance")
+var d = prompt("quel est votre numero")
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+
